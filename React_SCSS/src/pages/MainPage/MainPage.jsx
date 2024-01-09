7b76a2edc0fe2f1bf9aa1c48ddec62f6384fca17
Erik <erik.raboczki98@gmail.com>
2024-01-09 16:53:45 +0100

    This as long as we could progress at the first sprint.

---
+++ React_SCSS/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,23 @@
+export default function MainPage() {
+  return (
+    <>
+      <header className="navbar">
+        
+      </header>
+      
+      <main className="container">
+      <div className="logo"></div>
+        <div className="box-1"></div>
+        <div className="box-2"></div>
+        <div className="box-3"></div>
+        <div className="box-4"></div>
+      </main>
+      <footer className="footer">
+        <div className="box-1"></div>
+        <div className="box-2"></div>
+        {/* <div className="box-3"></div>
+        <div className="box-4"></div> */}
+      </footer>
+    </>
+  );
+}
